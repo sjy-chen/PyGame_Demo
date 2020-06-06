10f01c48a444ec42c82f305ed8e4099125589665
sjy-chen <samueljychen@gmail.com>
2020-06-06 16:18:36 -0400

    adding_character

--- scrollDemo.py
+++ scrollDemo.py
@@ -5,7 +5,9 @@ setAutoUpdate(False)
 
 setBackgroundImage("dungeonFloor.png")
 
-testSprite = makeSprite("links.gif", 32)
+testSprite = makeSprite("Main_Character_Front.png")
+addSpriteImage(testSprite, "Main_Character_Left.png")
+addSpriteImage(testSprite, "Main_Character_Right.png")
 
 lander = makeSprite("landerCrash.png")
 
@@ -32,36 +34,32 @@ hideLabel(start)
 
 frame = 0
 while True:
-
-    moveSprite(lander, xPos, yPos, True)
-    showSprite(lander)
-
-    if clock() > nextFrame:  # We only animate our character every 80ms.
-        frame = (frame + 1) % 8  # There are 8 frames of animation in each direction
-        nextFrame += 8  # so the modulus 8 allows it to loop
+    if not found:
+        moveSprite(lander, xPos, yPos, True)
+        showSprite(lander)
 
     if keyPressed("right"):
-        changeSpriteImage(testSprite, 0 * 8 + frame)  # 0*8 because right animations are the 0th set in the sprite sheet
+        changeSpriteImage(testSprite, 2)  # 0*8 because right animations are the 0th set in the sprite sheet
         scrollBackground(-5, 0)  # The player is moving right, so we scroll the background left
         xSpeed -= 5
 
     elif keyPressed("down"):
-        changeSpriteImage(testSprite, 1 * 8 + frame)  # down facing animations are the 1st set
+        changeSpriteImage(testSprite, 0)  # down facing animations are the 1st set
         scrollBackground(0, -5)
         ySpeed -= 5
 
     elif keyPressed("left"):
-        changeSpriteImage(testSprite, 2 * 8 + frame)  # and so on
+        changeSpriteImage(testSprite, 1)  # and so on
         scrollBackground(5, 0)
         xSpeed += 5
 
     elif keyPressed("up"):
-        changeSpriteImage(testSprite, 3 * 8 + frame)
+        changeSpriteImage(testSprite, 0)
         scrollBackground(0, 5)
         ySpeed += 5
 
     else:
-        changeSpriteImage(testSprite, 1 * 8 + 5)  # the static facing front look
+        changeSpriteImage(testSprite, 0)  # the static facing front look
 
     xPos += xSpeed
     xSpeed = 0
